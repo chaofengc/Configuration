2627f5dc475f56654f89a1eabfe775656f797d96
fly-cfchen <chaofenghust@gmail.com>
2019-02-18 20:10:42 +0800

    Update install script.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ ln -sf $HOME/Configuration/.tmux.conf $HOME/.tmux.conf
 # ---------------- Install vim plugin -------------------                                                                                                                                          
 if ! [ -d $HOME/.vim/plugged ]; then
     echo "Install vim plugin"
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     vim +PlugInstall +qall
 else
     echo "Vim plugin already installed."
